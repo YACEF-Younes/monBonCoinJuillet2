eb6d9b5c5a9808b4eb3d02b948a7d7c71125aa39
Csylvainc <campos.sylvain@gmail.com>
2023-07-25 15:26:46 +0200

    create htaccess

---
+++ Public/index.php
@@ -0,0 +1,9 @@
+<?php
+// ce fichier est le point d'entrée de notre site
+
+echo "point d'entrée";
+// pour rester sur le fichier index.php quoi qu'il arrive, je doit faire une réecriture d'url
+// Une des possibilités est d'utiliser un fichier de config du serveur apache qui s'appelle
+// .htaccess
+// Nous allons créer ce fichier dans le repertoire "public"
+// Nous allons aussi créer un virtualHost
