c0e7c85834f4d12bc7fcf96d6093cfd25174c07e
YACEF-Younes <138567041+YACEF-Younes@users.noreply.github.com>
2023-07-26 14:42:02 +0200

    detail product detailler

--- Views/products/detailProduct.php
+++ Views/products/detailProduct.php
@@ -1,14 +1,14 @@
-<?php if($erreur !="") : ?>
+<?php if ($erreur != "") : ?>
     <div class="alert alert-dismissible alert-danger">
-  <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-  <p><?= $erreur?></p>
-</div>
+        <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+        <p><?= $erreur ?></p>
+    </div>
 <?php else : ?>
     <div class="card">
         <div class="card-header bg-primary text-center">
             <h3><?= $product['productTitle'] ?></h3>
         </div>
-        <div class="card-body text-center">
+        <div class="card-body text-center ">
             <div class="row">
                 <div class="col-12 col-md-6">
                     <img src="/image/<?= $product['image'] ?>" alt="$product['productTitle']">
@@ -16,8 +16,23 @@
                 <div class="col-12 col-md-6">
                     <iframe src="https://www.google.com/maps?q=<?= $user['city'] ?>&t=&z=13&ie=UTF8&iwloc=&output=embed" width='100%' height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="p-2"></iframe>
                 </div>
-                
+
             </div>
+            <div class="row">
+                <div class="col-12 col-md-6 bg-dark">
+                    <p>Description :</p>
+                    <p><?= $product['description'] ?></p>
+                </div>
+                <div class="col-12 col-md-6 bg-dark">
+                    <p>Prix :</p>
+                    <p><?= $product['price'] ?> €</p>
+                </div>
+
+
+            </div>
+        </div>
+        <div class="card-footer text-center">
+            <a href="/panier" class="btn btn-secondary">Ajouter au panier</a>
         </div>
     </div>
 <?php endif  ?>
